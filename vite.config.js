cac119a03fc0390041c75582677e6793dd289a29
herik-erbeth <herik.docarmo@hotmail.com>
2025-01-31 23:32:34 -0300

    chore: add proxy configuration to vite.config.js for mock server

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,24 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Client port
-    host: true
+    proxy: {
+      '/pizzas': {
+        target: 'http://localhost:5100', // Mock server port
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the target:', req.method, req.url);
+          });
+          proxy.on('proxyReq', (proxyRes, req, _res) => {
+            console.log('Received Response from the target:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
   }
 })
