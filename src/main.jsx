51824bdec2b68f23c28dcc7c039234e77d696c00
herikerbeth <herik.docarmo@hotmail.com>
2025-02-03 18:39:08 -0300

    feat: integrate Material UI theme provider
    Wrap the application with ThemeProvider and CssBaseline to apply Material UI's global styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme();
+
 import Pizza from './Pizza.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Pizza />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Pizza className="Pizza"/>
+    </ThemeProvider>
   </StrictMode>,
 )
